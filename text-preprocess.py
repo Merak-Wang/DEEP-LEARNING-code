91acdf171359d79d62b9f03d4ea527003b968a96
MFwang <wang-mingfeng@qq.com>
2023-10-04 18:41:47 +0800

    4w

---
+++ text-preprocess.py
@@ -0,0 +1,111 @@
+import collections
+import re
+from d2l import torch as d2l
+
+#文本预处理
+#把词变成可训练的tensor
+#第一步，导入《Time Mechine》这本书
+#将数据集读取到多条文本组成的列表中
+d2l.DATA_HUB['time_machine'] = (d2l.DATA_URL + 'timemachine.txt',
+                               '090b5e7e70c295757f55df93cb0a180b9691891a')
+def read_time_machine():
+    """将时间机器数据集加载到文本行的列表中"""
+    with open(d2l.download('time_machine'), 'r') as f:
+        #读取每一行
+        lines = f.readlines()
+    #把非大写、小写字母的东西全部变成空格，去掉回车，大写变小写（暴力预处理）
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+lines = read_time_machine()
+
+#将一段文字转化成token，词元
+def tokenize(lines,token = 'word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print("错误：未知令牌类型 "+token)
+tokens = tokenize(lines)
+
+#构建一个字典，将字符串映射到从0开始的索引中
+class Vocab:
+    """文本词表"""
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        # 按出现频率排序
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1],reverse=True)
+        # 未知词元的索引为0
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):  # 未知词元的索引为0
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+def count_corpus(tokens):
+    """统计词元的频率"""
+    # 这里的tokens是1D列表或2D列表
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        # 将词元列表展平成一个列表
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+
+def load_corpus_time_machine(max_tokens=-1):
+    """返回时光机器数据集的词元索引列表和词表"""
+    lines = read_time_machine()
+    tokens = tokenize(lines, 'char')
+    vocab = Vocab(tokens)
+    # 因为时光机器数据集中的每个文本行不一定是一个句子或一个段落，
+    # 所以将所有文本行展平到一个列表中
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
+
+corpus, vocab = load_corpus_time_machine()
+len(corpus), len(vocab)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
